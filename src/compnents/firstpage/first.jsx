6264ad08a676ea0d6f356390412c7f8f4b4db4d6
akash <akashsrma3695@gmail.com>
2023-04-25 02:01:36 -0700

    first cmt

--- src/compnents/firstpage/first.jsx
+++ src/compnents/firstpage/first.jsx
@@ -4,6 +4,13 @@
 import promotion from "../../assets/img.jpg"
 import offer from "../../assets/OIP.jpg"
 import per from "../../assets/th.jpg"
+import people from "../../assets/people offer.jpg"
+import peoplee from "../../assets/good.png"
+import ding from "../../assets/ding.jpg"
+import special from "../../assets/specialoffer.jpg"
+import "./first.css"
+
+
 
 const Firstpage =()=>{
     return(
@@ -14,10 +21,24 @@ const Firstpage =()=>{
 
 
        </div>
+       <div>
+        <img src={people} alt="people" className="people"/>
+        <img src={peoplee} alt="peoplee" className="peoplee"/>
+        <img src={ding} alt="peopleee" className="peopleee"/>
+        <img src={special} alt="special" className="specialoffer"/>
+        
+
+
+       </div>
        <div className="about">
           <img src={offer} alt="offer" className="offer"></img>
          <img src={per} alt="per" className="per"></img>
      </div>
+
+     <div className="background">
+        <p className="us">ABOUT_US</p>
+     </div>
+
 
      
  </>
